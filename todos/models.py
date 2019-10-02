462e9fa49973bd57fab1077e1cdcc9715ea9ace1
changho <ckdghdus@naver.com>
2019-10-02 14:48:35 +0900

    모델 작성

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    # 데이터를 저장할 colums을 만들어 준다.
+    title = models.CharField(max_length=50)
+    due_date = models.DateField()
+    # 실제로 저장은 너가해
